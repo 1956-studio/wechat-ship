86680963256c0c856c3a2e52692cd8ef82d770e2
LiuNaidi <weinipingfan@gmail.com>
2014-08-25 21:06:29 +0800

    update db config test

--- client/test/db.test.js
+++ client/test/db.test.js
@@ -3,18 +3,6 @@ var dbconf = require('../db/db.js');
 var assert = require('assert');
 
 describe('dbconf', function() {
-	it('read', function(done) {
-		dbconf.read(null, function (err, result) {
-			assert.equal(err, null, 'err is not null');
-			done();
-		});
-	});
-	it('read', function(done) {
-		dbconf.read({"timeout":2000}, function (err, result) {
-			result.should.have.property('timeout', 2000);
-			done();
-		});
-	});
 	it('save', function(done) {
 		var default_config = {
 			redis: {
@@ -35,8 +23,32 @@ describe('dbconf', function() {
 			done();
 		});
 	});
-	it('removeById', function(done) {
+	it('read', function(done) {
+		dbconf.read(null, function (err, result) {
+			assert.equal(err, null, 'err is not null');
+			done();
+		});
+	});
+	it('read', function(done) {
+		dbconf.read({"timeout":2000}, function (err, result) {
+			result.should.have.property('timeout', 2000);
+			done();
+		});
+	});
+	it('readById', function(done) {
+		dbconf.readById("53f9efdb9dd7d87d13d1fc12", function (err, result) {
+			result.should.have.property('timeout', 2000);
+			done();
+		});
+	});
+	it('removeByIdNull', function(done) {
 		dbconf.removeById("53f27d311535cebc11847ba7", function (err) {
+			assert.equal(err, null, 'err is not null');
+			done();
+		});
+	});
+	it('removeByIdNotNull', function(done) {
+		dbconf.removeById("53f9eb3cd990556911ee6ed7", function (err) {
 			assert.equal(err, null, 'err is not null');
 			done();
 		});
