466e5140319fa2f98dce5b748f2ad3e6f2d18a2f
LiSheep <ltcmailhome@gmail.com>
2014-08-26 20:48:51 +0800

    add server's view

---
+++ client/routes/server.js
@@ -0,0 +1,35 @@
+var serverController = require("../controllers/server");
+
+var server = {};
+
+// ALL: /server/startup
+server.startup = function (req, res) {
+	if(req.method == "POST") {
+		var port = Number(req.body.port);
+		if(isNaN(port)) {
+			res.writeHead(400);
+			res.end("port is not number");
+		}else {
+			res.redirect("/server/liststatus")
+			serverController.startup(port);
+		}
+	}else if(req.method == "GET") {
+		res.render("server/startup");
+	}else {
+		res.writeHead(400);
+		res.end();
+	}
+}
+
+// GET: /server/liststatus
+server.listStatus = function (req, res) {
+	serverController.list(function(err, result){
+		var m_result = {
+			result:result
+		}
+		res.render("server/listStatus", m_result);
+	});
+	
+}
+
+module.exports = server;
