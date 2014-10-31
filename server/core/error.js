df0077aa30b34f49242d70f2cd9003928902074d
LiSheep <ltcmailhome@gmail.com>
2014-10-31 16:13:53 +0800

    add default error message

--- server/core/error.js
+++ server/core/error.js
@@ -6,15 +6,28 @@ var error = {}
 
 var errorInfo;
 
+var defaultError = new Array();
+defaultError.push(new errosModel({tag: 'syserr', message: '系统错误'}));
+defaultError.push(new errosModel({tag: 'msgnull', message: '消息为空'}));
+defaultError.push(new errosModel({tag: 'regerr', message: '已经注册过了'}));
+
 error.init = function(){
 	errosModel.Find({}, function (err, doc) {
 		if(err) {
-			console.log(err);
-		}else {
+			return console.log(err);
+		}else if(!doc || doc.length == 0) {
+			for(var i in defaultError) {
+				defaultError[i].Save();
+			}
+			doc = defaultError;
+		}
+		else {
 			errorInfo = doc;
 			console.log('error info init ok!');
 		}
 	});
+	
+	
 }
 
 error.get = function(key) {
